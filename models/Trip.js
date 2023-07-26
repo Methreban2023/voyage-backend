02a4c03b86300f959cfa67b7630bbc093aec0270
methreban2023 <alrefai.dalia@gmail.com>
2023-07-26 22:55:27 +0300

    check

--- models/Trip.js
+++ models/Trip.js
@@ -5,12 +5,11 @@ const tripSchema = new Schema(
     title: { type: String, required: true },
     description: String,
     tripDate: Date,
-    image: [
-      {
-        type: String,
-        default: "/",
-      },
-    ],
+    image: {
+      type: String,
+      default: "/",
+    },
+
     createdBy: { type: Schema.Types.ObjectId, ref: "User" },
     // relations
     cities: [{ type: Schema.Types.ObjectId, ref: "City" }],
