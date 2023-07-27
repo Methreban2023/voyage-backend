4c1fe682d62f01e785c65fa61abf34f952dd25d8
methreban2023 <alrefai.dalia@gmail.com>
2023-07-27 20:51:25 +0300

    profile

--- api/Profile/profile.controller.js
+++ api/Profile/profile.controller.js
@@ -2,9 +2,9 @@ const User = require("../../models/User");
 
 exports.getProfile = async (req, res, next) => {
   try {
-    // const { userId } = req.params;
-
-    const profile = await User.findById(req.user._id).populate(trips);
+    const profile = await User.findById(req.user._id)
+      .populate("trips")
+      .select("-password");
     res.status(200).json(profile);
   } catch (error) {
     next(error);
