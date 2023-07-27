b699915844cc8986de710114b0767b4db865c01c
methreban2023 <alrefai.dalia@gmail.com>
2023-07-27 12:22:16 +0300

    update profile

--- api/Profile/profile.routes.js
+++ api/Profile/profile.routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { getProfile } = require("../Profile/profile.controllers");
+const { getProfile, updateProfile } = require("./profile.controller");
 const router = express.Router();
 const passport = require("passport");
 const { param } = require("../../utils/params/param");
 
 // router.get("/:userId", getOneProfileById);
 router.get("/", getProfile);
-
+router.put("/", updateProfile);
 module.exports = router;
